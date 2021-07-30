71556794b53b8a94149fca77c781bd151bdc6b00
kwok1993 <warrengyj@163.com>
2021-07-30 16:56:43 +0800

    引入WarmUp和LabelSmoothingCrossEntropy

--- train.py
+++ train.py
@@ -4,33 +4,39 @@ __time__        = '2021/6/22 17:35'
 """
 import torch
 import torch.nn as nn
+from torch.optim.lr_scheduler import StepLR, ExponentialLR
 from torchvision import transforms, models
 from torch.utils.data import DataLoader
-from utils.torch_utils import select_device
-from utils.folder import ImageFolder
+from utils import select_device, ImageFolder, LabelSmoothingCrossEntropy, GradualWarmUpScheduler
 import os
 from tqdm import tqdm
 import argparse
 import logging
+import json
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(pathname)s->%(funcName)s[line:%(lineno)d] %(levelname)s | %(message)s')
 logger = logging.getLogger(__name__)
 
 
 def train():
-    labels = opt.labels
-    model_weights_path = opt.weights
-    epochs = opt.epochs
-    save_path = opt.save_path
-    path = opt.dataset_path
-    batch_size = opt.batch_size
+    model, labels, model_weights_path, epochs, save_path, path, batch_size = \
+        opt.model, opt.labels, opt.weights, opt.epochs, opt.save_path, opt.dataset_path, opt.batch_size
+
+    class_num = len(labels)
     # 查找可用 GPU
     device = select_device(device=opt.device, batch_size=batch_size)
 
     train_transform = transforms.Compose([
         # 图片随机裁剪为不同的大小和宽高比, 再统一尺寸
-        transforms.RandomResizedCrop(224),
+        # transforms.RandomResizedCrop(224),
+        # 图片统一尺寸
+        transforms.Resize(256),
+        # 中间区域裁剪
+        transforms.CenterCrop(224),
         # 图片水平翻转, 默认概率为 0.5
         transforms.RandomHorizontalFlip(),
+        # 图片垂直翻转, 默认概率为 0.5
+        transforms.RandomVerticalFlip(),
         # 将图片数据转为Tensor
         transforms.ToTensor(),
         # 数据标准化
@@ -50,6 +56,13 @@ def train():
     # 加载训练集图片数据
     # torchvision 实现的 ImageFolder 是读取训练集子目录名 sort() 排序后作为 class name, 没法自定义class index, 所以自己重写一个
     train_dataset = ImageFolder(root=os.path.join(path, 'train'), transform=train_transform, labels=labels)
+    # {'black': 0, 'blue': 1, 'gray': 2, 'red': 3, 'yellow': 4}
+    labels = train_dataset.class_to_idx
+    # 将标签索引与标签存进 json 文件
+    cls_dict = dict((val, key) for key, val in labels.items())
+    with open('./labels/{}.json'.format(path.split('/')[-1]), 'w') as f:
+        f.write(json.dumps(cls_dict, indent=4))
+
     # 打乱图片数据，并根据 batch_size 分批
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataset = ImageFolder(root=os.path.join(path, 'val'), transform=val_transform, labels=labels)
@@ -70,7 +83,7 @@ def train():
     '''
     # 使用 torchvision 做迁移训练,使用 models.resnet50(),就下载 resnet50 对应的预训练模型,以此类推
     # 如果想程序自动下载预训练模型，则使用 model = models.resnet50(pretrained=True), 然后根据下载路径获取预训练模型
-    model = models.resnet50()
+    model = eval('models.' + model + '()')
     # 多 GPU 训练时，需要将模型并行化，需要使用 DataParallel 来操作
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
@@ -84,15 +97,20 @@ def train():
     in_channel = model.fc.in_features
     # 预训练模型默认的 out_features 为 ImageNet 的 1000 分类
     # 替换预训练模型最后全连接层的输出特征（out_features）, 即自己数据集 number_classes,
-    model.fc = torch.nn.Linear(in_channel, out_features=2)
+    model.fc = torch.nn.Linear(in_channel, out_features=class_num)
     # 修改后的模型加载到指定 GPU
     model.to(device)
     # 损失函数： resnet 分类问题选用交叉熵损失函数
-    loss_function = nn.CrossEntropyLoss()
+    # loss_function = nn.CrossEntropyLoss()
+    loss_function = LabelSmoothingCrossEntropy().cuda()
     # 优化器
     # 尽管 Adam（自适应优化算法） 在训练集上的 loss 更小，训练阶段前期收敛更快，但是在测试集上的 loss 比 SGD（随机梯度下降算法） 高
     # optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+    scheduler_step_lr = StepLR(optimizer, step_size=10)
+    scheduler_warm_up = GradualWarmUpScheduler(optimizer, multiplier=1, total_epoch=5, after_scheduler=scheduler_step_lr)
+
     # 最佳准确率初始化
     best_acc = 0.0
     # batch 训练次数, train_loader 是根据 batch_size 做好分批的
@@ -145,6 +163,9 @@ def train():
                 # 更新本次 batch 验证结果
                 val_bar.desc = 'valid epoch[{}/{}]'.format(epoch + 1, epochs)
 
+        # 调整学习率
+        scheduler_warm_up.step()
+
         val_num = len(val_dataset)
         val_accurate = acc / val_num
         logger.info('[epoch {}] train_loss: {:.3f}, val_accuracy: {:.3f}'.format(epoch + 1, train_loss / train_steps, val_accurate))
@@ -159,12 +180,14 @@ def train():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='resnet50',
+                        help='支持resnet18,resnet34,resnet50,resnet101,resnet152,resnext50_32x4d,resnext101_32x8d,wide_resnet50_2,wide_resnet101_2')
     parser.add_argument('--weights', type=str, default='weights/resnet50-19c8e357.pth', help='预训练模型路径')
     parser.add_argument('--device', default='', help='可使用的GPU, 支持输入 0 或 0,1,2,3 或 cpu')
     parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--save-path', type=str, default='weights/my_resnet50.pt', help='训练完成后模型保存路径')
-    parser.add_argument('--dataset-path', type=str, default='../dataset', help='数据集路径')
+    parser.add_argument('--save-path', type=str, default='weights/uniform_resnet50.pt', help='训练完成后模型保存路径')
+    parser.add_argument('--dataset-path', type=str, default='../dataset/uniform', help='数据集路径')
     parser.add_argument('--batch-size', type=int, default=16, help='批量大小')
-    parser.add_argument('--labels', type=list, default=['dog', 'cat'], help='数据集标签')
+    parser.add_argument('--labels', type=list, default=['black', 'blue', 'gray', 'red', 'yellow'], help='数据集标签')
     opt = parser.parse_args()
     train()
