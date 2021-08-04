81fccff6a118995ea6cd9a597b630ee0ec7f1de4
kwok1993 <warrengyj@163.com>
2021-08-04 15:26:56 +0800

    更新readme

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * Warm Up
 * LabelSmoothingCrossEntropy
+* ImbalancedDatasetSampler
---
+++ test.py
@@ -0,0 +1,6 @@
+"""
+__author__      = 'kwok'
+__time__        = '2021/7/28 11:12'
+"""
+
+from torchvision.models.detection import FasterRCNN
